240f5cc62fbdd2f4f248c39c797d012f57787e1f
Mariana Guzmán <mggzmn91@gmail.com>
2023-02-27 12:01:06 -0600

    Selecting by text

--- cypress/e2e/first-test.cy.js
+++ cypress/e2e/first-test.cy.js
@@ -1,6 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
+/// <reference types="Cypress" />
+describe('task page', () => {
+  it('should render the main image', () => {
     cy.visit('http://127.0.0.1:5173/');
-    cy.get('li').should('have.length',6);
-  })
-})
+    cy.get('.main-header img');
+  });
+  it('should display the page title',()=>{
+    cy.visit('http://127.0.0.1:5173/');
+    cy.get('h1').contains('React');
+  });
+});
